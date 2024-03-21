1f305f3db1587252f0e2babad781eb52d0d7fc12
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-03-21 15:19:06 +0600

    set routing

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Nav from "../components/Nav";
+
+const MainLayout = () => {
+    return (
+        <div>
+            <div className="h-16">
+                <Nav></Nav>
+            </div>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default MainLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Blogs from './pages/Blogs.jsx';
+import Bookmarks from './pages/Bookmarks.jsx';
+import MainLayout from './layouts/MainLayout.jsx';
+import Home from './pages/Home.jsx';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: '/',
+        element:<Home></Home>
+      },
+      {
+        path: '/blogs',
+        element: <Blogs></Blogs>
+      },
+      {
+        path: '/bookmarks',
+        element: <Bookmarks></Bookmarks>
+      }
+    ]
+  },
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
