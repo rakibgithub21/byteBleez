5c7acf7ac7d0d2ce726d5c5d6ae695dcd4c4a77c
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-03-21 16:41:24 +0600

    home section done with animated svg

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../components/Nav";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
     return (
@@ -7,7 +8,10 @@ const MainLayout = () => {
             <div className="h-16">
                 <Nav></Nav>
             </div>
-            <Outlet></Outlet>
+            <div className='min-h-[calc(100vh-116px)]'>
+                <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
-
+import Hero from "../components/Hero";
+import wave from '../assets/wave.svg'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="flex relative flex-col justify-center items-center min-h-[calc(100vh-116px)]">
+            <Hero></Hero>
+                <img className="absolute bottom-0 w-full" src={wave} alt="" />
         </div>
     );
 };
