7361c7bbcb8c1f32c57c106a3dcf6a87edf667f3
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-03-22 14:33:14 +0600

    set cart information

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,16 +1,18 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
+import placeholder from '../assets/404.jpg'
 
 
 const BlogCard = ({ blog }) => {
-    console.log(blog);
+    // console.log(blog);
+    const { cover_image, title, description, published_at, id } = blog;
     return (
-        <Link to='/' className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900">
-            <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?1" />
+        <Link to={`/blog/${id}`} className="max-w-sm transition border-2 hover:scale-105 mx-auto group hover:no-underline border-primary hover:border-secondary border-opacity-30 focus:no-underline dark:bg-gray-900">
+            <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src={cover_image || placeholder} />
             <div className="p-6 space-y-2">
-                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
-                <span className="text-xs dark:text-gray-400">January 21, 2021</span>
-                <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
+                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">{title}</h3>
+                <span className="text-xs dark:text-gray-400">{ new Date(published_at).toLocaleDateString()}</span>
+                <p>{ description}</p>
             </div>
         </Link>
     );
